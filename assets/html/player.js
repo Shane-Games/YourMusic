075e9d66f1af3a09944d19b386538f33ffff2a62
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-12-06 08:27:35 -0500

    Added test song and play pause :) .

---
+++ assets/html/player.js
@@ -0,0 +1,13 @@
+let song = document.getElementById("audio");
+
+function togglePlay() {
+    if (song.paused) {
+        song.play();
+    } else {
+        song.pause();
+    }
+};
+
+function volume() {
+
+}
