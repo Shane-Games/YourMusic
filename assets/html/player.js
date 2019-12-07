b8aabde15a2babcd324e7dad8b169df13cfb169e
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-12-06 21:58:25 -0500

    The big one update.

--- assets/html/player.js
+++ assets/html/player.js
@@ -1,13 +1,31 @@
 let song = document.getElementById("audio");
+let playPauseBtn = document.getElementById("playerBtn");
+let time = document.getElementById("time");
+let volumeSlider = document.getElementById("volume");
+
+volumeSlider.value = "50";
 
 function togglePlay() {
-    if (song.paused) {
-        song.play();
-    } else {
-        song.pause();
-    }
-};
-
-function volume() {
-
+  if (song.paused) {
+    song.play();
+    playPauseBtn.src = "pause.png";
+  } else {
+    song.pause();
+    playPauseBtn.src = "play2.png";
+  }
 }
+
+volumeSlider.addEventListener("input", function() {
+  song.volume = this.value / 100;
+});
+
+time.addEventListener("input", function() {
+  valctime = song.currentTime / 100;
+  song.currentTime = this.value;
+  console.log(song.currentTime);
+});
+
+function getTime() {
+  time.value += 0.1;
+}
+setInterval(getTime, 1000);
