9454391083605779c356fe6eb2828ce630275360
Shane_Games <45543346+Shane-Games@users.noreply.github.com>
2019-12-07 13:27:21 -0500

    The functional update.

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const {
-    app,
-    BrowserWindow
-} = require('electron')
-let path = require('path');
+const { app, BrowserWindow, Menu } = require("electron");
+let path = require("path");
 
 function createWindow() {
-    // Create the browser window.
-    let win = new BrowserWindow({
-        icon: path.join(__dirname, './assets/icons/icon.ico')
+  // Create the browser window.
+  let win = new BrowserWindow({
+    icon: path.join(__dirname, "./assets/icons/icon.ico")
+  });
 
-    })
-
-    // and load the index.html of the app.
-    win.removeMenu()
-    win.loadFile('./assets/html/index.html');
+  win.loadFile("./assets/html/index.html");
+  win.setMenu(null);
+  Menu.setApplicationMenu(null);
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
